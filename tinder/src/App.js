991f66c924c6aadf07d398871c20691dcdde511a
Kertich <kipsangkertich@gmail.com>
2021-10-19 09:44:36 +0300

    update tinder clone

---
+++ tinder/src/App.js
@@ -0,0 +1,18 @@
+import Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="app">
+   
+
+    <Header />
+     {/* TinderCards */}
+     {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
